23abb9f87bc6fd6f4a77e2e40b35f22da4717ca4
avijitchoudhury <avijit.choudhury24@gmail.com>
2019-09-26 19:36:45 +0000

    added the mocha chai testing file

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,17 +1,26 @@
 const request = require('request');
-let args = process.argv[2];
-let URL = `https://api.thecatapi.com/v1/breeds/search?q=${args}`;
+// let args = process.argv[2];
+// let URL = `https://api.thecatapi.com/v1/breeds/search?q=${args}`;
 
+const fetchBreedDescription = function(breedName, callBack) {
+let URL = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
 request(URL, (error, response, body) => {
   if (error) {
-    console.log("URL ERROR");
+    callBack(error, response);
   } else {
     const data = JSON.parse(body);
     if (data.length < 1) {
-      console.log("This kitty does not exist.");
+      callBack(null, "This kitty does not exist.");
     } else {
-      console.log((data[0].description));
+      callBack(null, (data[0].description.trim()));
     }
   }
 });
+
+};
+
+
+module.exports = { fetchBreedDescription };
+
+
 
